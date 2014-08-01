7950384076e2cae93159cea972301d7040be7936
arirux <fdopen@ml.ignorelist.com>
2014-08-01 23:48:30 +0200

    update godi-custom_printf

--- godi/godi-custom_printf/files/oasis_myocamlbuild.ml
+++ godi/godi-custom_printf/files/oasis_myocamlbuild.ml
@@ -618,7 +618,16 @@ let dispatch = function
     flag ["ocaml"; "compile"; tag] & S[A"-ppopt"; A file];
     flag ["ocaml"; "ocamldep"; tag] & S[A"-ppopt"; A file];
     flag ["ocaml"; "doc"; tag] & S[A"-ppopt"; A file];
-    dep ["ocaml"; "ocamldep"; tag] [file]
+    dep ["ocaml"; "ocamldep"; tag] [file];
+
+    let env = BaseEnvLight.load () in
+    let ver = BaseEnvLight.var_get "ocaml_version" env in
+    let ver = Scanf.sscanf ver "%d.%d" (fun major minor -> (major, minor)) in
+    if ver >= (4, 02) then begin
+      flag ["ocaml"; "compile"; "use_macro"] & S[A"-ppopt"; A "-DOCAML_4_02"];
+      flag ["ocaml"; "ocamldep"; "use_macro"] & S[A"-ppopt"; A "-DOCAML_4_02"];
+    end
+
   | _ ->
     ()
 
