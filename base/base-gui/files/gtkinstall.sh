0b7b36cb78f2a024bd22a9517291aaf61bf347f4
arirux <fdopen@ml.ignorelist.com>
2015-01-08 17:43:22 +0100

    update base-gui

--- base/base-gui/files/gtkinstall.sh
+++ base/base-gui/files/gtkinstall.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-wrksrc="$1"
+dir="$1"
 
-if [ $# -ne 1 ]; then
-    echo "wrong argument count" >&2
+if [ ! -d "$dir" ]; then
+    echo "dir missing :(" >&2
     exit 1
 fi
 
-mkdir -p "$wrksrc"
-
-godi_gui_dir="${wrksrc}/gui"
-mkdir -p "$godi_gui_dir"
+godi_gui_dir="${dir}/gui"
 godi_root_dir="$(dirname "$(godi_confdir)")"
 godi_bin_dir="${godi_root_dir}/bin"
 
@@ -21,95 +18,32 @@ if [ -z "$OBJDUMP" ]; then
     echo "OBJDUMP not defined" >&2
     exit 1
 fi
-if [ -z "$STRINGS" ]; then
-    echo "STRINGS not defined" >&2
-    exit 1
-fi
 
 if ! mkdir -p "${godi_gui_dir}/bin" \
     "${godi_gui_dir}/etc" \
+    "${godi_gui_dir}/lib" \
     "${godi_gui_dir}/share/themes" 
 then
     exit 1
 fi
 
 
-#1: found
-#2: checked
-declare -A mymap
-
-handle_dll(){
-    dll=$1
-    new_found=1
-    while read -r ndll ; do
-	if [ -z "$ndll" ] || [ ! -f "${godi_bin_dir}/${ndll}" ] ; then
-            continue
-        fi
-        mapstatus="${mymap[$ndll]}"
-        if [ "$mapstatus" = "1" ] || [ "$mapstatus" = "2" ]; then
-	    continue
-	fi
-	mymap[$ndll]=1
-        new_found=0
-        cp -p "${godi_bin_dir}/${ndll}" "${godi_gui_dir}/bin"
-    done < <( ( $OBJDUMP -p "${dll}" | grep 'DLL Name:' | awk '{print $3}' ; $STRINGS "${dll}" | grep -i '.dll$' ) | sort -u )
-    return $new_found
-}
-
 cdir="$(pwd)"
-for d in etc/pango etc/gtk-2.0 lib/gdk-pixbuf-2.0 lib/pango lib/libglade lib/gtk-2.0 ; do
-    mkdir -p "${godi_gui_dir}/${d}"
-    cd "${godi_root_dir}/${d}"
-    godi_pax -rw -pp . "${godi_gui_dir}/${d}"
-    while read -r -d $'\0' dll ; do
-        handle_dll "${dll}" || true
-    done< <(find "${godi_gui_dir}/${d}" -type f -iname '.dll' -print0)
-    find "${godi_gui_dir}/${d}" -type f -iname '*.h' -delete
-done
-
-cd "${godi_bin_dir}"
-for f in \
-    libgtk-win32*.dll \
-    libgtksourceview*.dll \
-    libgnomecanvas*.dll \
-    libcurl*.dll \
-    libglade-2*.dll ;\
-do
-    if [ -z "$f" ] || [ ! -f "$f" ] || [ -f "${godi_gui_dir}/bin/${f}" ]; then
-        continue
-    fi
-    mymap[$f]=1
-    cp -p "${f}" "${godi_gui_dir}/bin"
-done
-
-cd "$cdir"
-
-new_found=1
-while [ $new_found -eq 1 ]; do
-    new_found=0
-    for dll in ${!mymap[@]} ; do
-	value=${mymap[$dll]}
-	if [ "$value" = "1" ]; then
-	    mymap[$dll]=2
-            if handle_dll "${godi_bin_dir}/${dll}" ; then
-                new_found=1
-            fi
-        fi
-    done
-done
 
 cd "${godi_bin_dir}"
 cp -p gspawn-win*-helper*.exe "${godi_gui_dir}/bin"
-
 cd "$cdir"
 
 cp -a "${godi_root_dir}/etc/fonts" "${godi_gui_dir}/etc"
+cp -a "${godi_root_dir}/etc/pango" "${godi_gui_dir}/etc"
 cp -a "${godi_root_dir}/etc/gtk-2.0" "${godi_gui_dir}/etc"
+cp -a "${godi_root_dir}/lib/gtk-2.0" "${godi_gui_dir}/lib"
+cp -a "${godi_root_dir}/lib/gdk-pixbuf-2.0" "${godi_gui_dir}/lib"
+cp -a "${godi_root_dir}/lib/pango" "${godi_gui_dir}/lib"
+cp -a "${godi_root_dir}/lib/libglade" "${godi_gui_dir}/lib"
 cp -a "${godi_root_dir}/share/themes/MS-Windows" "${godi_gui_dir}/share/themes"
+cp -a "${godi_root_dir}/share/gtksourceview-2.0" "${godi_gui_dir}/share"
 
-cp -a \
-    "${godi_root_dir}/share/gtksourceview-2.0" \
-    "${godi_gui_dir}/share"
 
 cd "${godi_gui_dir}/share/gtksourceview-2.0/language-specs/"
 for f in *.* ; do
@@ -123,6 +57,72 @@ for f in *.* ; do
     esac
 done
 cd "$cdir"
+
+
+#1: found
+#2: checked
+declare -A mymap
+
+cd "${godi_bin_dir}"
+for f in \
+    libgobject*.dll \
+    libglib*.dll \
+    libgtk-win32*.dll \
+    libgdk*.dll \
+    libpango*.dll \
+    libgtksourceview*.dll \
+    libglade*.dll \
+    libgnomecanvas*.dll \
+    libgtkspell*.dll \
+    librsvg*.dll \
+    libcurl*.dll
+do
+    [ ! -f "$f" ] && continue
+    mymap[$f]=1
+done
+cd ~-
+
+list_dlls(){
+    $OBJDUMP -p "$1" | /bin/grep 'DLL Name:' | /bin/awk '{print $3}'
+    $STRINGS "$1" | /bin/grep '.dll$'
+}
+
+
+for dir in "${godi_root_dir}/lib/gdk-pixbuf-2.0" "${godi_root_dir}/lib/pango" "${godi_root_dir}/lib/libglade" "${godi_root_dir}/lib/gtk-2.0" ; do
+    while read -r -d $'\0' dll ; do
+        if [ -z "$dll" ] || [ ! -f "$dll" ]; then
+            continue
+        fi
+        mymap[$dll]=1
+    done< <(/usr/bin/find "$dir" -type f -iname '.dll' -print0)
+done
+
+oldIFS=$IFS
+IFS='
+'
+new_found=1
+while [ $new_found -eq 1 ]; do
+    new_found=0
+    for dll in ${!mymap[@]} ; do
+	value=${mymap[$dll]}
+	if [ $value -eq 1 ]; then
+	    mymap[$dll]=2
+            if [ -f "${godi_bin_dir}/${dll}" ]; then
+	        cp -p "${godi_bin_dir}/${dll}" "${godi_gui_dir}/bin"
+            fi
+	    for ndll in $(list_dlls "${godi_bin_dir}/${dll}") ; do
+		if [ -z "$ndll" ] || [ ! -f "${godi_bin_dir}/${ndll}" ] \
+		    || [ "${mymap[$ndll]}" = "1" ] || [ "${mymap[$ndll]}" = "2" ] ; then
+		    continue
+		fi
+		mymap[$ndll]=1
+		new_found=1
+	    done
+	fi
+    done
+done
+
+IFS=$oldIFS
 
 find "${godi_gui_dir}" -type l -delete
 find "${godi_gui_dir}" -type f -name '*~' -delete
